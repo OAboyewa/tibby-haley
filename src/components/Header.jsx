4141a39ceb0774ecb3ef7e53aaae6d6ef490d806
OAboyewa <oluwatobiaboyewa@gmail.com>
2023-08-02 23:59:30 +0100

    building header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "/src/styles/Header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <a href="">
+                <span>Tibby Haley</span>
+            </a>
+            <ul>
+                <li>Work</li>
+                <li><a href="">Bio</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </header>
+    );
+}
