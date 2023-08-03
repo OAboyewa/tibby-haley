63173b4c106af45e0b3fa2fce48ec22a7bb8a1bb
OAboyewa <oluwatobiaboyewa@gmail.com>
2023-08-03 23:56:48 +0100

    working on hover dropdown menu for header

---
+++ src/components/Dropdown-List.jsx
@@ -0,0 +1,12 @@
+import "/src/styles/Header.css";
+
+export default function DropdownList() {
+    return (
+        <div className="list-cnt">
+            <a href="">Citrus Season</a>
+            <a href="">Room With a View</a>
+            <a href="">True Romance</a>
+            <a href="">Power Suit</a>
+        </div>
+    );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import "/src/styles/Header.css";
+import "/src/styles/Text.css";
+import DropdownList from "/src/components/Dropdown-List.jsx";
 
 export default function Header() {
+    useEffect(() => {
+        const element = document.querySelector("div.list-cnt");
+        element.addEventListener("mouseleave", () => {
+            
+        })
+    }, []);
+
     return (
         <header>
-            <a href="">
-                <span>Tibby Haley</span>
-            </a>
-            <ul>
-                <li>Work</li>
+            <span>
+                <a href="" className="ft-primary h3 color-primary">Tibby Haley</a>
+            </span>
+            <ul className="nav-menu-cnt ft-secondary color-primary">
+                <li className="dropdown-cnt">
+                    <span>Work</span>
+                    <DropdownList ></DropdownList>
+                </li>
                 <li><a href="">Bio</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
