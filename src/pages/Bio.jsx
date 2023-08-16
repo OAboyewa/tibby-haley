5ce5edaf6d92ea8456d8286f64f7412874b6b288
OAboyewa <oluwatobiaboyewa@gmail.com>
2023-08-16 20:29:25 +0100

    working on bio page

---
+++ src/pages/Bio.jsx
@@ -0,0 +1,28 @@
+import "/src/styles/Bio.css";
+
+export default function Bio() {
+    return(
+        <div>
+            <div></div>
+            <div>
+                <p>
+                    <span>About Tibby Haley</span>
+                    <span></span>
+                    <span>
+                        I'm a paragraph. Click here to add your own text and edit me. 
+                        It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. 
+                        Feel free to drag and drop me anywhere you like on your page. 
+                        I’m a great place for you to tell a story and let your users know a little more about you.
+                    </span>
+                    <span>
+                        This is a great space to write a long text about your company and your services. 
+                        You can use this space to go into a little more detail about your company. 
+                        Talk about your team and what services you provide. 
+                        Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. 
+                        Make your company stand out and show your visitors who you are.
+                    </span>
+                </p>
+            </div>
+        </div>
+    );
+}
